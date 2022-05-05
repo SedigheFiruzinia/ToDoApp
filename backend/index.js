e45239903766bcf810b9a77e46b76cb2ab61f62d
Sedi <sedi@Sedis-MacBook-Pro.local>
2022-05-05 16:01:51 +0300

    modify backend structure

--- backend/index.js
+++ backend/index.js
@@ -1,38 +1,9 @@
-const express = require("express");
+const app = require("./app"); // the actual Express application
+const http = require("http");
 const logger = require("./utils/logger");
-const app = express();
-const fs = require("fs");
-require("dotenv").config();
+const config = require("./utils/config");
 
-app.use(express.static("build"));
-app.use(express.json());
-
-app.get("/tasks", (req, res) => {
-  fs.readFile("./db.json", "utf8", (err, jsonString) => {
-    if (err) {
-      logger.error("Error reading file from disk:", err);
-      return;
-    }
-    try {
-      const db = JSON.parse(jsonString);
-      logger.info("First task is:", db.tasks); //=> returns first task
-      res.json(db);
-    } catch (err) {
-      logger.error("Error parsing JSON string:", err);
-    }
-  });
+const server = http.createServer(app);
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`);
-});
-
-const errorHandler = (error, request, response, next) => {
-  logger.error(error.message);
-  if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message });
-  }
-  next(error);
-};
-app.use(errorHandler);
