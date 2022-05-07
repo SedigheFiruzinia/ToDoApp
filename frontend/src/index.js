541c5f59c725c3160e60476f0fef67b48bcd31fd
Sedi <sedi@Sedis-MacBook-Pro.local>
2022-05-07 13:21:22 +0300

    use configureStore instead of createStore

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,22 +1,18 @@
 import React from "react";
 import * as ReactDOMClient from "react-dom/client";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware, combineReducers } from "redux";
-import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { configureStore } from "@reduxjs/toolkit";
 import App from "./App";
 import tasksReducer from "./reducers/taskReducer";
 import notificationReducer from "./reducers/notificationReducer";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const reducer = combineReducers({
-  Tasks: tasksReducer,
-  Notification: notificationReducer,
+const store = configureStore({
+  reducer: {
+    Tasks: tasksReducer,
+    Notification: notificationReducer,
+  },
 });
-
-const composeEnhancer = composeWithDevTools({ trace: true });
-
-const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)));
 
 ReactDOMClient.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
